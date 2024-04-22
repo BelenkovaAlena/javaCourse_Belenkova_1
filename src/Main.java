0b8a25ed88f5d35cfb8f301e7de149808d886406
user <user@24-003057.iteco-inno.ru>
2024-04-22 14:00:20 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        String st_1 = "_______________'\"________";
+        String st_2 = "______________---________";
+        String st_3 = "__________/\\_|_|_________";
+        String st_4 = "_________/__\\|_|_________";
+        String st_5 = "________/____\\_|_________";
+        String st_6 = "_______/______\\|_________";
+        String st_7 = "______/__[__]__\\_________";
+        String st_8 = "_____/|________|\\________";
+        String st_9 = "____/_|________|_\\_______";
+        String st_10 = "______|__----__|_________";
+        String st_11 = "______|__| |__|_________";
+        System.out.println(st_1);
+        System.out.println(st_2);
+        System.out.println(st_3);
+        System.out.println(st_4);
+        System.out.println(st_5);
+        System.out.println(st_6);
+        System.out.println(st_7);
+        System.out.println(st_8);
+        System.out.println(st_9);
+        System.out.println(st_10);
+        System.out.println(st_11);
+    }
+}
